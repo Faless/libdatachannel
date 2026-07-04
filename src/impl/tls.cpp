96bf3f549506ceb66122b64572afd1e8d0e99ef5
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2026-07-04 11:07:42 +0200

    Add support for mbedTLS 4.1, use PSA Crypto API

--- src/impl/tls.cpp
+++ src/impl/tls.cpp
@@ -96,6 +96,15 @@ size_t my_strftme(char *buf, size_t size, const char *format, const time_t *t) {
 
 namespace rtc::mbedtls {
 
+void init() {
+	psa_crypto_init();
+}
+
+int random_func(void *rng, unsigned char *out, size_t len) {
+	(void)rng;
+	return psa_generate_random(out, len);
+}
+
 // Return false on non-fatal error
 bool check(int ret, const string &message) {
 	if (ret < 0) {
